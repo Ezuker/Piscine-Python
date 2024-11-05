fd82f183f453a90a379a605085e494a388338663
Ezuker <benoit.carolle.pro@gmail.com>
2024-11-05 16:05:22 +0100

    day01 finished

--- day01/ex05/pimp_image.py
+++ day01/ex05/pimp_image.py
@@ -4,20 +4,54 @@ from load_image import ft_load
 import matplotlib.pyplot as plt
 
 
-def ft_invert(array) -> array:
+def ft_invert(array: list) -> list:
 	"""
 	Inverts the color of the image received.
 	"""
+	new_arr = [[255 - array[i][j] for j in range(len(array[0]))] for i in range(len(array))]
+	np_array = np.array(new_arr)
+	im = Image.fromarray(np_array)
+	im.show()
+	return np_array
 	
 
-def ft_red(array) -> array:
+def ft_red(array: list) -> list:
+	np_array = np.array(array)
+	np_array[:, :, 1] = 0
+	np_array[:, :, 2] = 0
+	np_array = np_array.astype(np.uint8)
+	im = Image.fromarray(np_array)
+	im.show()
+	return np_array
 
 
-def ft_green(array) -> array:
+def ft_green(array: list) -> list:
+	np_array = np.array(array)
+	np_array[:, :, 0] = 0
+	np_array[:, :, 2] = 0
+	np_array = np_array.astype(np.uint8)
+	im = Image.fromarray(np_array)
+	im.show()
+	return np_array
 
 
-def ft_blue(array) -> array:
+def ft_blue(array: list) -> list:
+	np_array = np.array(array)
+	np_array[:, :, 0] = 0
+	np_array[:, :, 1] = 0
+	np_array = np_array.astype(np.uint8)
+	im = Image.fromarray(np_array)
+	im.show()
+	return np_array
 
 
-def ft_grey(array) -> array:
-
+def ft_grey(array: list) -> list:
+	"""
+	Apply grey filter
+	"""
+	np_array = np.array(array)
+	np_array = np.mean(np_array, axis=2)
+	np_array = np_array.astype(np.uint8)
+	im = Image.fromarray(np_array)
+	im.show()
+	return np_array
